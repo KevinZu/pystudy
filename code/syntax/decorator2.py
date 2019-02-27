3d350ad52f52bb6475553aa82e13fae6dcd5f001
kevinzu <zukeqiang@foxmail.com>
2019-02-27 10:18:08 +0800

    decorator

---
+++ code/syntax/decorator2.py
@@ -0,0 +1,19 @@
+
+from functools import wraps
+ 
+def logit(func):
+    @wraps(func)
+    def with_logging(*args, **kwargs):
+        print(func.__name__ + " was called")
+        return func(*args, **kwargs)
+    return with_logging
+ 
+@logit
+def addition_func(x):
+   """Do some math."""
+   return x + x
+ 
+ 
+result = addition_func(4)
+# Output: addition_func was called
+
